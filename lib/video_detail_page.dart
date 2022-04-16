a6ffba2066defc89b821e727a50d0268fdc49548
TakutoYoshizumi <y.takuto.jp@gmail.com>
2022-04-16 11:04:45 +0900

    Drawer追加

--- lib/video_detail_page.dart
+++ lib/video_detail_page.dart
@@ -11,6 +11,57 @@ class VideoDetailPage extends StatelessWidget {
           child: Text('動画'),
         ),
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              height: 100,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      "https://img.icons8.com/color/2x/youtube-play.png",
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Text(
+                          "YouTube",
+                          style: TextStyle(
+                              fontFamily: "Roboto",
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
